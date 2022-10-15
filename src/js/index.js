7d9ac805c902622931d87219dbd45ffb8316ffe6
Renata Bernucio <47673970+renata-bernucio@users.noreply.github.com>
2022-10-14 22:19:23 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const abas = document.querySelectorAll('.aba');
+
+abas.forEach(aba => { //forEach percorre cada item da lista
+    
+    aba.addEventListener('click', function() {
+
+        if(aba.classList.contains('selecionada')){
+            return;
+        }
+
+        selecionarAba(aba)
+        mostrarInformacoesDaAba(aba)
+        
+    })
+})
+
+function selecionarAba(aba) {
+    
+    const abaSelecionada = document.querySelector('.aba.selecionada')
+    abaSelecionada.classList.remove('selecionada')
+    aba.classList.add('selecionada')
+
+}
+
+function mostrarInformacoesDaAba(aba) {
+    const informacaoSelecionada = document.querySelector('.informacao.selecionada')
+    informacaoSelecionada.classList.remove('selecionada')
+    
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    informacaoASerMostrada.classList.add('selecionada')
+}
